96f0122b8dfbec311581afe535be130285a9364e
Osawa Naotaka <ohsawa.naotaka@gmail.com>
2025-12-02 23:37:21 +0900

    フロントエンドMVP実装中

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -6,6 +6,7 @@ import type { Task } from "./types";
 export function Home(): JSX.Element {
     const biz = useRef<Business>(null);
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [title, setTitle] = useState<string>("");
 
     useEffect(() => {
         const p = new Persistent();
@@ -13,41 +14,68 @@ export function Home(): JSX.Element {
         setTasks(biz.current.tasks);
     }, []);
 
-    function handleAddTask(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        const form = event.currentTarget;
-        const input = form.elements.namedItem("task") as HTMLInputElement;
-        const title = input.value.trim();
+    function handleAddTask(): void {
         if (title && biz.current) {
             setTasks(biz.current.create({ title }));
-            input.value = "";
+            setTitle("");
         }
     }
 
-    function handleEditTask(taskId: string, newTitle: string) {
+    function handleEditTask(task: Task, newTitle: string): void {
         if (biz.current) {
-            const task = tasks.find((t) => t.id === taskId);
-            if (!task) throw new Error("Task not found");
-
             task.data.title = newTitle;
             setTasks(biz.current.edit(task));
         }
     }
 
     return (
-        <main>
-            <h1>VanishToDo</h1>
-            <form onSubmit={handleAddTask}>
-                <input type="text" name="task" placeholder="New Task" />
-                <button type="submit">Add</button>
-            </form>
-            <ul>
-                {tasks.map((task) => (
-                    <li key={task.id}>
-                        <input type="text" defaultValue={task.data.title} onInput={(e) => handleEditTask(task.id, e.currentTarget.value)} />
-                    </li>
-                ))}
-            </ul>
-        </main>
+        <>
+            <header>
+                <div className="responsive">
+                    <h1>VanishToDo</h1>
+                    <button type="button">
+                        <img src="asset/icon/bars.svg" alt="menu" className="icon" />
+                    </button>
+                </div>
+            </header>
+            <main className="responsive">
+                <form className="card" onSubmit={handleAddTask}>
+                    <input type="text" name="task" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="New Task" />
+                    <div className="row">
+                        <button type="submit">＋ 追加</button>
+                        <input type="radio" name="task" id="task-light" defaultChecked />
+                        <label htmlFor="task-light">軽</label>
+                        <input type="radio" name="task" id="task-medium" />
+                        <label htmlFor="task-medium">中</label>
+                        <input type="radio" name="task" id="task-heavy" />
+                        <label htmlFor="task-heavy">重</label>
+                        <input type="radio" name="task" id="task-duedate" />
+                        <label htmlFor="task-duedate">締切</label>
+                    </div>
+                </form>
+                <ul>
+                    {tasks.map((task) => (
+                        <li key={task.id} className="card">
+                            <input type="checkbox" name="item" id={task.id} />
+                            <input type="text" defaultValue={task.data.title} onInput={(e) => handleEditTask(task, e.currentTarget.value)} />
+                        </li>
+                    ))}
+                </ul>
+            </main>
+            <footer>
+                <div className="responsive">
+                    <input type="radio" name="tab" id="tab-all" />
+                    <label htmlFor="tab-all">すべて</label>
+                    <input type="radio" name="tab" id="tab-light" defaultChecked />
+                    <label htmlFor="tab-light">軽</label>
+                    <input type="radio" name="tab" id="tab-medium" />
+                    <label htmlFor="tab-medium">中</label>
+                    <input type="radio" name="tab" id="tab-heavy" />
+                    <label htmlFor="tab-heavy">重</label>
+                    <input type="radio" name="tab" id="tab-duedate" />
+                    <label htmlFor="tab-duedate">締切</label>
+                </div>
+            </footer>
+        </>
     );
 }
