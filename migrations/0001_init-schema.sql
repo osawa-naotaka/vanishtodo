cd67c69adc071735e1baf0c15428358f07c9bf23
Osawa Naotaka <ohsawa.naotaka@gmail.com>
2025-12-08 20:31:42 +0900

    初期マイグレーション（db初期化）を追加

--- migrations/0001_init-schema.sql
+++ migrations/0001_init-schema.sql
@@ -1,3 +1,4 @@
+-- Migration number: 0001 	 2025-12-08T11:24:00.645Z
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, user_name TEXT);
 INSERT INTO users (user_id, user_name) VALUES (1, 'TAIGA Ruli');
