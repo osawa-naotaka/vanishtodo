e901dd3d67b14567610e462e518786adb52a14b6
Osawa Naotaka <ohsawa.naotaka@gmail.com>
2025-12-16 20:39:00 +0900

    ドロワーのためにハンバーガーメニューをcheck boxに変更

--- src/layer/Presentation/AppBar.tsx
+++ src/layer/Presentation/AppBar.tsx
@@ -4,9 +4,10 @@ export function AppBar(): JSX.Element {
     return (
         <header className="app-bar">
             <div className="responsive">
-                <button type="button" className="app-bar-menu-button">
-                    <img src="asset/icon/bars.svg" alt="menu" className="icon" />
-                </button>
+                <input type="checkbox" name="menu-toggle" id="menu-toggle" />
+                <label htmlFor="menu-toggle" className="app-bar-menu-button">
+                    <img src="asset/icon/bars.svg" alt="menu" className="app-bar-menu-icon" />
+                </label>
                 <h1 className="app-bar-title">VanishToDo</h1>
             </div>
         </header>
